2b78659d8fbc4d855710a9897ae9ddc1af6e2998
Srivathsan <srivaths311@gmail.com>
2018-10-01 10:27:55 +0000

    Changed commands to case insensitive
    Test1

--- js/terminal.js
+++ js/terminal.js
@@ -1,29 +1,38 @@
 jQuery(function($, undefined){
     $('#term_demo').terminal(function(command){
-        if (command == 'social'){
+        if (command.toUpperCase() === 'SOCIAL'){
             this.echo("Email : srivats64@gmail.com \n")
             this.echo("Github Repo : https://github.com/froossv \n")
             this.echo("Instagram : https://instagram.com/vathzenmode \n")
             this.echo("Blog : https://themaraudersmapforlife.wordpress.com")
+            this.echo("Linkedin : https://www.linkedin.com/in/sri-vathsan-0112358")
         }
 
-        if (command == 'edu'){
+        if (command.toUpperCase() === 'EDU'){
             this.echo("School : Kendriya Vidyalaya Ashok Nagar\n")
             this.echo("College : SASTRA Deemed to be University\n")
         }
 
-        if (command == 'help'){
+        if (command.toUpperCase() === 'HELP'){
             this.echo("Type Commands like social,info,works,blog etc. For Complete List refer Readme.md. \n")
         }
 
-        if(command == 'goto blog'){
+        if(command.toUpperCase() === 'GOTO BLOG'){
             this.echo("Opening in New Tab...")
             window.open('https://themaraudersmapforlife.wordpress.com','_blank')
         }
 
-        if(command == 'info'){
+        if(command.toUpperCase() === 'INFO'){
             this.echo("Hello User! Im Srivathsan studying B.Tech Information and Communication in SASTRA University, \n")
             this.echo("Thanjavur,India. Highly passionate about Cryptography and Mathematics")
+        }
+        
+        if(command == '42'){
+            this.echo("The Answer to the Life, Universe and Everything.\n")
+        }
+        
+        if(command.toUpperCase() === 'WHAT DOES THE FOX SAY'){
+            this.echo("Ring Ding Ring Ding Ringadingading")
         }
 
         else {
